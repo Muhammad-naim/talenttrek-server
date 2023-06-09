461ebc60ca5a11eb0ea27044f94c06a869b3ae78
naim <naimulislam0927@gmail.com>
2023-06-09 17:07:33 +0600

    added vercel configuration

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
+      // await client.connect();
       const courseCollection = client.db('talendtrekDB').collection('courses');
       const bannerCollection = client.db('talendtrekDB').collection('bannerData');
       const instructorCollection = client.db('talendtrekDB').collection('instructors');
